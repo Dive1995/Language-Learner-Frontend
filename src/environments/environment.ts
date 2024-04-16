b7c7ed6fa071d2455f22b2e81bab933410e9b07f
Diveshan <diveshan1995@gmail.com>
2024-04-16 20:07:16 +0530

    removed sensitive date

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-   production: false,
-   GOOGLE_CLIENT_ID: "359242762858-nvonhpunhku3r8t0412j14ohup3kucvl.apps.googleusercontent.com",
-   GOOGLE_API_KEY: "AIzaSyBxUgYL7hQ5mPhienY4QFtsO9UgOqvxRHI"
-};
